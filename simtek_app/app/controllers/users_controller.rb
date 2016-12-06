3602b6d71921a4946678b53afab2d43906b074ef
greer184 <greer.184@osu.edu>
2016-12-06 07:22:31 -0500

    Finalized user model and allow password updates

--- simtek_app/app/controllers/users_controller.rb
+++ simtek_app/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @admin = @user.admin
@@ -10,6 +13,8 @@ class UsersController < ApplicationController
 
   def create 
     @user = User.new(user_params)
+    @user.admin = false
+    @user.subscription = "None"
     if @user.save
       flash[:success] = "Welcome to Simtek. Sign in to Start." 
       redirect_to root_path
@@ -17,11 +22,38 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Changes successful"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation, :admin)
+                                 :password_confirmation, :admin,
+                                 :subscription)
+  end
+
+  def logged_in_user
+    unless user_logged_in?
+      flash[:danger] = "Log in to edit profile"
+      redirect_to root_path
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless @user == current_user
   end
 end
